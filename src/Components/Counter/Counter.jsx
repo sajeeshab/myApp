600d5d53b7c56fb557bc29b73e88ffae3d8dc8f5
sajeeshab <officialsajeesh@gmail.com>
2023-12-03 01:36:55 +0530

    Initial Counter App

---
+++ src/Components/Counter/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+
+function Counter() {
+  const [state, setState] = useState(0);
+
+  const decrement = () => {
+    setState(state - 1);
+  };
+  const increment = () => {
+    setState(state + 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter: {state}</h1>
+      <Button className="bg-danger m-2" onClick={decrement}>
+        -
+      </Button>
+      <Button className="bg-success m-2" onClick={increment}>
+        +
+      </Button>
+    </div>
+  );
+}
+
+export default Counter;
