c7854a074ce84f0945aa2f4f5b7b5330f120e71f
sajeeshab <officialsajeesh@gmail.com>
2024-01-08 21:15:25 +0530

    Reset button added

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,25 +1,46 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { Button } from "react-bootstrap";
 import "./Counter.css";
 
-function Counter() {
-  const [state, setState] = useState(0);
+const initialState = 0;
 
-  const decrement = () => {
-    setState(state - 1);
-  };
-  const increment = () => {
-    setState(state + 1);
-  };
+const reducer = (state = initialState, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return (state = 0);
+    default:
+      return state;
+  }
+};
+
+function Counter() {
+  // const [state, setState] = useState(0);
+
+  // const decrement = () => {
+  //   setState(state - 1);
+  // };
+  // const increment = () => {
+  //   setState(state + 1);
+
+  // };
+
+  const [count, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
-      <h1>Counter: {state}</h1>
-      <Button className="bg-danger m-2" onClick={decrement}>
+      <h1>Counter: {count}</h1>
+      <Button className="bg-danger m-2" onClick={() => dispatch("decrement")}>
         -
       </Button>
-      <Button className="bg-success m-2" onClick={increment}>
+      <Button className="bg-success m-2" onClick={() => dispatch("increment")}>
         +
+      </Button>
+      <Button className="bg-info" onClick={() => dispatch("reset")}>
+        Reset
       </Button>
       <br />
       <br />
